f22626a84acfc475caf47d53243c48da8c5b8b3c
Alexey-t <support@uvviewsoft.com>
2018-05-17 14:45:22 +0300

    for last CudaTree

--- __init__.py
+++ __init__.py
@@ -1,5 +1,37 @@
-from .find_headers import get_headers
+import itertools
 
-helper = {
-  'WikidPad': get_headers,
-}
+
+def _is_pre(s, ch, need_space):
+    if not s.startswith(ch):
+        return
+    r = len(list(itertools.takewhile(lambda c: ch == c, s)))
+    if not need_space:
+        return r
+    if r < len(s) and s[r].isspace():
+        return r
+
+
+def is_line_head(s):
+    return _is_pre(s, '+', True)
+
+
+def get_headers(filename, lines):
+    '''
+    Generates headers in format:
+    line_index, header_level, header_text
+    '''
+    tick = False
+    for i, s in enumerate(lines):
+        if not s.strip():
+            continue
+        if s.lstrip().startswith('<<'):
+            tick = True
+            continue
+        if s.lstrip().startswith('>>'):
+            tick = False
+            continue
+        if tick:
+            continue
+        r = is_line_head(s)
+        if r:
+            yield i, r, s[r:].strip()
